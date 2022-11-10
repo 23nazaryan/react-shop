545d2354265f9fef1337ef1b0a57fdbbec5b8ba9
23nazaryan <23vahe@rambler.ru>
2022-11-10 19:03:29 +0400

    add github repo links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 function Footer() {
     return (<footer className="page-footer grey darken-4">
-                <div className="footer-copyright">
-                    <div className="container">
-                        © {new Date().getFullYear()} Copyright Text
-                        <a className="grey-text text-lighten-4 right" href="#">Repo</a>
-                    </div>
-                </div>
-            </footer>)
+        <div className="footer-copyright">
+            <div className="container">
+                © {new Date().getFullYear()} Copyright Text
+                <a className="grey-text text-lighten-4 right"
+                   target="_blank" href="https://23nazaryan.github.io/react-shop/" rel="noreferrer">Repo</a>
+            </div>
+        </div>
+    </footer>)
 }
 
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 function Header() {
     return (<div className="navbar-fixed">
-                <nav className="cyan darken-4">
-                    <div className="nav-wrapper">
-                        <a href="#" className="brand-logo">React Shop</a>
-                        <ul className="right hide-on-med-and-down">
-                            <li><a href="#">Repo</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>)
+        <nav className="cyan darken-4">
+            <div className="nav-wrapper">
+                <a href="/" className="brand-logo">React Shop</a>
+                <ul className="right hide-on-med-and-down">
+                    <li><a target="_blank" href="https://23nazaryan.github.io/react-shop/" rel="noreferrer">Repo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>)
 }
 
 export default Header
